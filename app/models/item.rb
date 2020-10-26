08ba6551a6812e72b8f53e4a496ee2d900bd1ae2
Mayuu Furuki <aaa08027744484@gmail.com>
2020-10-26 13:55:11 +0900

    Activehashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
   has_many :items
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :status
+  belongs_to_active_hash :delivery_fee
+  
+  with_options numericality: { other_than: 1 } do
+  validates :status_id
+  validates :delivery_fee
+  end
 end
--- db/migrate/20201025095514_create_items.rb
+++ db/migrate/20201025095514_create_items.rb
@@ -5,6 +5,11 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.text :text, null: false
       t.integer :price, null: false
       t.references :user, null: false, foreign_key: true
+      t.integer :status_id, null: false
+      t.integer :delivery_fee_id, null: false
+      t.integer :prefecture_id, null: false
+      t.integer :delivery_day_id, null: false
+      t.integer :category_id, null: false
       t.timestamps
     end
   end
