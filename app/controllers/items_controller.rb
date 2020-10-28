9d4566e582f1b3f7bf521b47e98732a7c968da71
Mayuu Furuki <aaa08027744484@gmail.com>
2020-10-28 15:43:21 +0900

    条件分岐の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -34,8 +34,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    if current_user.id == @item.user_id
     @item.destroy
     redirect_to root_path
+    else
+      rediret_to root_path
+    end
   end
 
   private
