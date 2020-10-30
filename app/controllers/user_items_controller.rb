042f55f7496bbe3dad04fb5a6a11b8e55268078b
Mayuu Furuki <aaa08027744484@gmail.com>
2020-10-30 18:06:15 +0900

    トークン情報の送信

--- app/controllers/user_items_controller.rb
+++ app/controllers/user_items_controller.rb
@@ -3,27 +3,33 @@ class UserItemsController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
     @buy = Buy.new
-    if current_user.id == @item.user_id || @item.user_item.present?
-      redirect_to root_path
-    end
+    redirect_to root_path if current_user.id == @item.user_id || @item.user_item.present?
   end
 
   def create
-   
-   @item = Item.find(params[:item_id])
-   @buy = Buy.new(buy_params)
-   binding.pry
-     if @buy.valid?
+    @item = Item.find(params[:item_id])
+    @buy = Buy.new(buy_params)
+    if @buy.valid?
+      pay_item
       @buy.save
       redirect_to root_path
-     else
+    else
       render action: :index
-     end
+    end
   end
 
   private
 
   def buy_params
-    params.permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :item_id).merge(user_id: current_user.id)
+    params.permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :item_id).merge(user_id: current_user.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_f22d69ffab5f395a67434a9e"
+      Payjp::Charge.create(
+      amount: @item.price,
+      card: params[:token],
+      currency: "jpy"
+      )
   end
 end
--- app/models/buy.rb
+++ app/models/buy.rb
@@ -1,14 +1,15 @@
 class Buy
   include ActiveModel::Model
-  attr_accessor :user_item_id, :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number
+  attr_accessor :user_item_id, :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :token
 
   with_options presence: true do
     validates :postal_code
     validates :city
     validates :address
-    validates :phone_number, format:{with: /\A\d{10}$|^\d{11}\z/}
+    validates :phone_number, format: { with: /\A\d{10}$|^\d{11}\z/ }
+    validates :token
   end
-  validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
   validates :prefecture_id, numericality: { other_than: 1 }
 
   def save
